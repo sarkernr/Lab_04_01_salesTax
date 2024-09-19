aedd7e4b0f777fb242826f2fef284ef21794d420
naomi <sarkernr@mail.uc.edu>
2024-09-19 13:56:09 -0400

    Completed!

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+       double purchasePrice = 350.50;
+       double salesTax = 0;
+       double SALES_TAX_RATE =.05;
+
+       salesTax = purchasePrice * SALES_TAX_RATE;
+
+       System.out.println("The sales tax on a purchase of " + purchasePrice + " is " + salesTax);
+    }
+}
